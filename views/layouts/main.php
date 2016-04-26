fcd4854f0df029ef988497466f1c157661268737
garry3peace <garry3peace@gmail.com>
2016-04-26 11:06:54 +0700

    # perbaiki salah ketik di google analytics

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -11,7 +11,7 @@ use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
 AppAsset::register($this);
-$this->registerJs("<script>
+$this->registerJs("
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -19,8 +19,7 @@ $this->registerJs("<script>
 
   ga('create', 'UA-75806776-1', 'auto');
   ga('send', 'pageview');
-
-</script>", View::POS_END, 'googleAnalytics');
+", View::POS_END, 'googleAnalytics');
 
 ?>
 <?php $this->beginPage() ?>
