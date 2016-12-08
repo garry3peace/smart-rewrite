f35d445103d6868acfae94b79badafc6f5477ae4
garry3peace <garry3peace@gmail.com>
2016-12-08 17:31:19 +0700

    optimasi js dan css

--- config/web.php
+++ config/web.php
@@ -11,6 +11,26 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+		'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'bundles' => [
+                        'yii\web\JqueryAsset' => [
+                            'js' => [
+                                YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                            ]
+                        ],
+                        'yii\bootstrap\BootstrapAsset' => [
+                            'css' => [
+                                YII_ENV_DEV ? 'css/bootstrap.css' : 'css/bootstrap.min.css',
+                            ]
+                        ],
+                        'yii\bootstrap\BootstrapPluginAsset' => [
+                            'js' => [
+                                YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+                            ]
+                        ]
+            ],
+        ],
         'request' => [
             'cookieValidationKey' => 'y1kB87_prXcWGfjAW5IlB2u1vMWYZfxB',
 			'hostInfo'=>'http://localhost:8080',
