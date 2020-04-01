e85b585f186395bc8c7268c908ea63681b11792c
Garry <garry@computesta.com>
2020-04-01 14:32:00 +0700

    Ubah database untuk Heroku

--- config/prod-db.php
+++ config/prod-db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=dha10000_smartrewrite',
-    'username' => 'dha10000_smart',
-    'password' => '(hr^,PsDp2rJ',
+    'dsn' => 'mysql:host=localhost;dbname=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'username' => 'z3k0lxi5fgqtq481',
+    'password' => 'n45kl294hztgmsqb',
     'charset' => 'utf8',
 ];
